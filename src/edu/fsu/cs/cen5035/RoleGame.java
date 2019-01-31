3de1de3fdb3348ba761a329a978bac3f503af7bc
TimothyBarao <timothy.barao@gmail.com>
2019-01-31 16:50:52 -0500

    Added test cases for SimpleHammer class
    Created test cases for the simple hammer weapon in RoleGame.java.
    Added two new armor variables to test medium and high values of armor.

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -8,17 +8,23 @@ public class RoleGame {
 
     public static void main(String[] args) {
 
-    	int armor = 29;
+    	int armor = 29, highArmor = 55, medArmor = 32;
 
         Weapon weapon = WeaponFactory.getWeapon("sword");
-        System.out.println("Sword has " + weapon.hit() + " of damage.");        
+        System.out.println("Sword has " + weapon.hit() + " of damage.");
         System.out.println("Swords was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
         weapon = WeaponFactory.getWeapon("spear");
-        System.out.println("Spear has " + weapon.hit() + " of damage.");        
+        System.out.println("Spear has " + weapon.hit() + " of damage.");
         System.out.println("Spear was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
-        
-        //TODO: Add the remainig weapons here
+
+        weapon = WeaponFactory.getWeapon("simple hammer");
+        System.out.println("Simple hammer has " + weapon.hit() + " of damage.");
+        System.out.println("Simple hammer was able to do " + weapon.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+        System.out.println("Simple hammer was able to do " + weapon.hit(medArmor) + " of damage due to an armor with "+medArmor+ " points.");
+        System.out.println("Simple hammer was able to do " + weapon.hit(highArmor) + " of damage due to an armor with "+highArmor+ " points.");
+
+        //TODO: Add the remaining weapons here
 
     }
 }
